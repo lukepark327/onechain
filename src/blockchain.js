e83e3d53abfc91ab11b14a8a3b99b61b2de00cd3
twodude <twodude@naver.com>
2019-02-13 02:45:04 +0900

    Update blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -39,8 +39,8 @@ class Block {
 function getGenesisBlock() {
     const version = "1.0.0";
     const index = 0;
-    const previousHash = "0000000000000000000000000000000000000000000000000000000000000000";
-    const timestamp = 1231006505;   // 01/03/2009 @ 6:15pm (UTC)
+    const previousHash = '0'.repeat(64);
+    const timestamp = 1231006505;  // 01/03/2009 @ 6:15pm (UTC)
     const difficulty = 0;
     const nonce = 0;
     const data = ["The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"];
