deb70f2fc80e4ae6ebdfa7867bd82fdbf749eec0
Luke Park <twodude@naver.com>
2019-05-02 04:37:30 +0900

    chapter-1 codes

--- src/main.js
+++ src/main.js
@@ -1,76 +1,136 @@
 "use strict";
-const express = require("express");
-const bodyParser = require("body-parser");
+const CryptoJS = require("crypto-js");
+const merkle = require("merkle");
 
-const nw = require("./network");
-const wl = require("./wallet");
-const ut = require("./utils");
-
-const http_port = process.env.HTTP_PORT || 3001;
-const initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
-
-function initHttpServer() {
-    const bc = require("./blockchain");
-
-    const app = express();
-    app.use(bodyParser.json());
-
-    app.get("/blocks", function (req, res) {
-        res.send(bc.getBlockchain());
-    });
-    app.post("/mineBlock", function (req, res) {
-        const data = req.body.data || [];
-        const newBlock = bc.mineBlock(data);
-        if (newBlock === null) {
-            res.status(400).send('Bad Request');
-        }
-        else {
-            res.send(newBlock);
-        }
-    });
-    app.get("/version", function (req, res) {
-        res.send(ut.getCurrentVersion());
-    });
-    app.post("/blockVersion", function (req, res) {
-        const index = req.body.index;
-        res.send(bc.getBlockVersion(index));
-    });
-    app.get("/peers", function (req, res) {
-        /**
-         * https://developer.mozilla.org
-         * 
-         * The map() method creates a new array
-         * with the results of calling a provided function
-         * on every element in the calling array.
-         */
-        res.send(nw.getSockets().map(function (s) {
-            return s._socket.remoteAddress + ':' + s._socket.remotePort;
-        }));
-
-        // var resStrings = [];
-        // nw.getSockets().forEach(function(s){
-        //     resStrings.push(s._socket.remoteAddress + ':' + s._socket.remotePort);
-        // });
-        // res.send(resStrings);
-    });
-    app.post("/addPeers", function (req, res) {
-        const peers = req.body.peers || [];
-        nw.connectToPeers(peers);
-        res.send();
-    });
-    app.get("/address", function (req, res) {
-        const address = wl.getPublicFromWallet().toString();
-        res.send({ "address": address });
-    });
-    app.post("/stop", function (req, res) {
-        res.send({ "msg": "Stopping server" });
-        process.exit();
-    });
-    app.listen(http_port, function () { console.log("Listening http port on: " + http_port) });
+class BlockHeader {
+    constructor(version, index, previousHash, timestamp, merkleRoot) {
+        this.version = version;
+        this.index = index;
+        this.previousHash = previousHash.toString().toUpperCase();
+        this.timestamp = timestamp;
+        this.merkleRoot = merkleRoot;
+    }
 }
 
-// main
-nw.connectToPeers(initialPeers);
-initHttpServer();
-nw.initP2PServer();
-wl.initWallet();
+class Block {
+    constructor(header, data) {
+        this.header = header;
+        this.data = data;
+    }
+}
+
+var blockchain = [];
+
+function getBlockchain() { return blockchain; }
+function getLatestBlock() { return blockchain[blockchain.length - 1]; }
+
+function getGenesisBlock() {
+    const version = "1.0.0";
+    const index = 0;
+    const previousHash = '0'.repeat(64);
+    const timestamp = 1231006505; // 01/03/2009 @ 6:15pm (UTC)
+    const data = ["The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"];
+
+    const merkleTree = merkle("sha256").sync(data);
+    const merkleRoot = merkleTree.root() || '0'.repeat(64);
+
+    const header = new BlockHeader(version, index, previousHash, timestamp, merkleRoot);
+    return new Block(header, data);
+}
+
+function generateNextBlock(blockData) {
+    const previousBlock = getLatestBlock();
+    const currentVersion = getCurrentVersion();
+    const nextIndex = previousBlock.header.index + 1;
+    const previousHash = calculateHashForBlock(previousBlock);
+    const nextTimestamp = getCurrentTimestamp();
+
+    const merkleTree = merkle("sha256").sync(blockData);
+    const merkleRoot = merkleTree.root() || '0'.repeat(64);
+
+    const newBlockHeader = new BlockHeader(currentVersion, nextIndex, previousHash, nextTimestamp, merkleRoot);
+    return new Block(newBlockHeader, blockData);
+}
+
+function getCurrentVersion() {
+    const fs = require("fs");
+
+    const packageJson = fs.readFileSync("./package.json");
+    const currentVersion = JSON.parse(packageJson).version;
+    return currentVersion;
+}
+
+function getCurrentTimestamp() {
+    return Math.round(new Date().getTime() / 1000);
+}
+
+function addBlock(newBlock) {
+    if (isValidNewBlock(newBlock, getLatestBlock())) {
+        blockchain.push(newBlock);
+        return true;
+    }
+    return false;
+}
+
+function calculateHash(version, index, previousHash, timestamp, merkleRoot) {
+    return CryptoJS.SHA256(version + index + previousHash + timestamp + merkleRoot).toString().toUpperCase();
+}
+
+function calculateHashForBlock(block) {
+    return calculateHash(
+        block.header.version,
+        block.header.index,
+        block.header.previousHash,
+        block.header.timestamp,
+        block.header.merkleRoot
+    );
+}
+
+function isValidNewBlock(newBlock, previousBlock) {
+    if (!isValidBlockStructure(newBlock)) {
+        console.log('invalid block structure: %s', JSON.stringify(newBlock));
+        return false;
+    }
+    else if (previousBlock.header.index + 1 !== newBlock.header.index) {
+        console.log("Invalid index");
+        return false;
+    }
+    else if (calculateHashForBlock(previousBlock) !== newBlock.header.previousHash) {
+        console.log("Invalid previousHash");
+        return false;
+    }
+    else if ((
+        newBlock.data.length === 0
+        && ('0'.repeat(64) !== newBlock.header.merkleRoot)
+    ) || (
+            newBlock.data.length !== 0
+            && (merkle("sha256").sync(newBlock.data).root() !== newBlock.header.merkleRoot)
+        )) {
+        console.log("Invalid merkleRoot");
+        return false;
+    }
+    return true;
+}
+
+function isValidBlockStructure(block) {
+    return typeof(block.header.version) === 'string'
+        && typeof(block.header.index) === 'number'
+        && typeof(block.header.previousHash) === 'string'
+        && typeof(block.header.timestamp) === 'number'
+        && typeof(block.header.merkleRoot) === 'string'
+        && typeof(block.data) === 'object';
+}
+
+function isValidChain(blockchainToValidate) {
+    if (JSON.stringify(blockchainToValidate[0]) !== JSON.stringify(getGenesisBlock())) {
+        return false;
+    }
+    var tempBlocks = [blockchainToValidate[0]];
+    for (var i = 1; i < blockchainToValidate.length; i++) {
+        if (isValidNewBlock(blockchainToValidate[i], tempBlocks[i - 1])) {
+            tempBlocks.push(blockchainToValidate[i]);
+        }
+        else { return false; }
+    }
+    return true;
+}
