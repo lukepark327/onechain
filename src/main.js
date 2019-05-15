d093a22ac65eff6e8550c9965a0ad83fa5f97dd5
Luke Park <twodude@naver.com>
2019-05-15 13:23:44 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -112,13 +112,10 @@ function isValidNewBlock(newBlock, previousBlock) {
         console.log('invalid timestamp');
         return false;
     }
-    else if ((
-        newBlock.data.length === 0
-        && ('0'.repeat(64) !== newBlock.header.merkleRoot)
-    ) || (
-            newBlock.data.length !== 0
-            && (merkle("sha256").sync(newBlock.data).root() !== newBlock.header.merkleRoot)
-        )) {
+    else if (
+        (newBlock.data.length !== 0 && (merkle("sha256").sync(newBlock.data).root() !== newBlock.header.merkleRoot))
+        || (newBlock.data.length === 0 && ('0'.repeat(64) !== newBlock.header.merkleRoot))
+    ) {
         console.log("Invalid merkleRoot");
         return false;
     }
@@ -419,6 +416,50 @@ function isValidTimestamp(newBlock, previousBlock) {
     return (previousBlock.header.timestamp - 60 < newBlock.header.timestamp)
         && newBlock.header.timestamp - 60 < getCurrentTimestamp();
 }
+
+// Chapter-4
+const ecdsa = require("elliptic");
+const ec = new ecdsa.ec("secp256k1");
+const fs = require("fs");
+
+const privateKeyLocation = "wallet/" + (process.env.PRIVATE_KEY || "default");
+const privateKeyFile = privateKeyLocation + "/private_key";
+
+function generatePrivateKey() {
+    const keyPair = ec.genKeyPair();
+    const privateKey = keyPair.getPrivate();
+    return privateKey.toString(16);
+}
+
+function initWallet() {
+    if (fs.existsSync(privateKeyFile)) {
+        console.log("Load wallet with private key from: %s", privateKeyFile);
+        return;
+    }
+
+    if (!fs.existsSync("wallet/")) { fs.mkdirSync("wallet/"); }
+    if (!fs.existsSync(privateKeyLocation)) { fs.mkdirSync(privateKeyLocation); }
+
+    const newPrivateKey = generatePrivateKey();
+    fs.writeFileSync(privateKeyFile, newPrivateKey);
+    console.log("Create new wallet with private key to: %s", privateKeyFile);
+}
+
+
+function getPrivateFromWallet() {
+    const buffer = fs.readFileSync(privateKeyFile, "utf8");
+    return buffer.toString();
+}
+
+function getPublicFromWallet() {
+    const privateKey = getPrivateFromWallet();
+    const key = ec.keyFromPrivate(privateKey, "hex");
+    return key.getPublic().encode("hex");
+}
+
+
+
+
 
 // main
 initHttpServer();
