38d4ee04f20ab983e15c4a6142a1951ef6eb3f07
Luke Park <twodude@naver.com>
2019-10-04 22:51:13 +0900

    add front(home)page

--- explorer/src/main.js
+++ explorer/src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
+import truncate from 'vue-truncate'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+Vue.prototype.$http = axios
+Vue.use(truncate)
 Vue.config.productionTip = false
 
 new Vue({
